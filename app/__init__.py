bb0b05b880d46bb3262f91a1c8799555a685e249
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-02-20 03:23:29 +0800

    Update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from app.routers import router
-from app.routers.login import pwd_context
-from app.database import fake_db as db
 
 app = FastAPI()
 
@@ -9,17 +7,5 @@ app = FastAPI()
 app.include_router(router)
 
 @app.get("/", include_in_schema=False)
-async def root():
+async def root() -> dict:
     return {"alive": True}
-
-# admin, for testing
-@app.get("/admin", include_in_schema=False)
-async def admin():
-    return db
-
-@app.post("/admin/users", include_in_schema=False)
-async def admin_users(email: str, password: str):
-    if len(email) == 0 or len(password) == 0:
-        return {"error": "email or password is empty"}
-    db["users"][email] = pwd_context.hash(password)
-    return {"message": "success"}
