ccb89d0b6238b37900620adf88bfdaac327c57b0
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-02-20 03:24:53 +0800

    Create app/models.py

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Text, JSON
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    email = Column("email", String(255), primary_key = True, nullable = False)
+    password = Column("pwd", String(60), nullable = False)
+
+class BlogPost(Base):
+    __tablename__ = "posts"
+    id = Column("id", String(16), primary_key = True, nullable = False)
+    title = Column("title", Text, nullable = False)
+    content = Column("content", Text)
+    tags = Column("tags", JSON)
