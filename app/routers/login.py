3e9314665e9490a61aa3eb649d0058e7062e3f04
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-02-20 03:26:27 +0800

    Update app/routers/login.py

--- app/routers/login.py
+++ app/routers/login.py
@@ -8,18 +8,27 @@ from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
 from dotenv import load_dotenv
-from app.database import fake_db as db
-from app.schemas import Token, TokenData
+from app.database import get_db
+from app import schemas, crud
 
-SECRET_KEY = token_hex(32)
-print(SECRET_KEY)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_DAYS = 30
 
 load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+if SECRET_KEY is None:
+    SECRET_KEY = token_hex(32)
+    with open(".env", "a", encoding="utf-8") as f:
+        f.write(f"SECRET_KEY={SECRET_KEY}\n")
+
 JWT_ISSUER = os.getenv("JWT_ISSUER")
-print(JWT_ISSUER)
+if JWT_ISSUER is None:
+    JWT_ISSUER = "fastapi"
+    with open(".env", "a", encoding="utf-8") as f:
+        f.write(f"JWT_ISSUER={JWT_ISSUER}\n")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -40,7 +49,10 @@ def create_jwt_token(user: str) -> str:
     }
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> dict:
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        db: Session = Depends(get_db)
+    ) -> dict:
     def credentials_exception(detail: str) -> HTTPException:
         return HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -50,33 +62,33 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> dic
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("email")
-        issuer: str = payload.get("iss")
+        email = payload.get("email")
+        issuer = payload.get("iss")
         if email is None:
             raise credentials_exception("Could not validate credentials")
         if issuer != JWT_ISSUER:
             raise credentials_exception("Invalid token issuer")
-        token_data = TokenData(email=email)
+        token_data = schemas.TokenData(email=email)
     except ExpiredSignatureError as e:
         raise credentials_exception("Token expired") from e
     except InvalidTokenError as e:
         raise credentials_exception("Invalid token") from e
 
-    user = db["users"].get(token_data.email, None)
+    user = crud.get_user(db, email=token_data.email)
     if user is None:
         raise credentials_exception("No such user")
     return user
 
 @router.post("")
-async def login(email: str, password: str) -> Token:
-    hash_pwd = db["users"].get(email)
-    if not hash_pwd:
+async def login(email: str, password: str, db: Session = Depends(get_db)) -> schemas.Token:
+    user = crud.get_user(db, email)
+    if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="No such user",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    if not pwd_context.verify(password, hash_pwd):
+    if not pwd_context.verify(password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect password",
@@ -84,4 +96,4 @@ async def login(email: str, password: str) -> Token:
         )
 
     token = create_jwt_token(email)
-    return Token(access_token=token, token_type="bearer")
+    return schemas.Token(access_token=token, token_type="bearer")
