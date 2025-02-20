4514c994ca9ca5bacab437f0bb3c6c854842d7b3
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-02-21 01:47:35 +0800

    Update app/database.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,12 @@
 import os
 from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
+
 DB_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DB_URL, echo=True)
